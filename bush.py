54dd4a480f5c3dd311810074128d22ef24b565a6
Matviyyyyyy <127428009+Matviyyyyyy@users.noreply.github.com>
2024-04-04 19:46:35 +0300

    + class bush and some change tank_hero

---
+++ bush.py
@@ -0,0 +1,7 @@
+class Bush:
+    def __init__(self, start_x, start_y, hits, filename):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.hits = hits
+        self.filename = filename
+
--- tank_hero.py
+++ tank_hero.py
@@ -1,8 +1,8 @@
 class Tank:
-    def __init__(self, start_x, start_y, img_file, speed):
+    def __init__(self, start_x, start_y, filename, speed):
         self.start_x = start_x
         self.start_y = start_y
-        self.img_file = img_file
+        self.filename = filename
         self.speed = speed
 
     def move_up(self):
@@ -13,3 +13,6 @@ class Tank:
 
     def move_left(self):
         self.start_x = self.start_x - self.speed
+
+    def move_right(self):
+        self.start_x = self.start_x + self.speed
