e31db65db5c54bd73d92011a4726dc0f60a49941
Matviyyyyyy <127428009+Matviyyyyyy@users.noreply.github.com>
2024-04-05 18:15:09 +0300

    + class bush and some change tank_hero

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from pygame import *
-import time
 
 init()
 screen = display.set_mode((600, 600))
-fps = time.Clock()
 display.set_caption("Battle City")
+clock = time.Clock()
+
 def showEndWindow(window, message):
     clock = time.Clock()
     run = True
-    font.init()
     while run:
         # обробка подій
         for e in event.get():
             if e.type == QUIT:
                 run = False
-            display.update()
+        display.update()
         clock.tick(60)
 
 running = True
 
 while running:
+    # обробка подій
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+
+    display.update()
+    clock.tick(60)
 
 
 
