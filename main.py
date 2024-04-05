ea5b2eb6005d4c7144499b7b319232d0cbce5448
Matviyyyyyy <127428009+Matviyyyyyy@users.noreply.github.com>
2024-04-05 18:53:30 +0300

    + class bush and some change tank_hero

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def showEndWindow(window):
 
 running = True
 
-tank_hero  = Tank(100, 100, 'tank_hero_2.png', 10, 50, 50)
+tank_hero  = Tank(100, 100, 'tank_hero_2.png', 3, 50, 50)
 
 background = transform.scale(image.load("background.jfif"), (600, 600))
 
