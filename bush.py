33a63f98a6bf556fdb2343b089c9e72772d17de3
newcomer123456 <157855469+newcomer123456@users.noreply.github.com>
2024-04-05 18:01:08 +0300

    +

--- bush.py
+++ bush.py
@@ -1,7 +1,10 @@
+import pygame
 class Bush:
-    def __init__(self, start_x, start_y, hits, filename):
-        self.start_x = start_x
-        self.start_y = start_y
+    def __init__(self, x, y, hits, filename):
+        self.x = x
+        self.y = y
         self.hits = hits
         self.filename = filename
+    def draw(self, window):
+        window.blit(self.filename, (self.rect.x, self.rect.y))
 
