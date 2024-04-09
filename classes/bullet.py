d7ac21dc2fc8e4a09594316fe1b60c5ad65a6301
Matviyyyyyy <127428009+Matviyyyyyy@users.noreply.github.com>
2024-04-09 23:18:13 +0300

    +folders +colderrect_wall +comments +changes class tank_hero

---
+++ classes/bullet.py
@@ -0,0 +1,29 @@
+from pygame import *
+
+class Bullet:
+    def __init__(self, x, y, filename, speed, width, height, angle):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.filename = filename
+        self.image = transform.scale(image.load(filename), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.angle = angle
+    def update(self):
+        if self.angle == 90:
+            self.rect.y -= self.speed
+        if self.angle == 180:
+            self.rect.x -= self.speed
+        if self.angle == 270:
+            self.rect.y += self.speed
+        if self.angle == 0:
+            self.rect.x += self.speed
+
+
+
+    def draw(self, window):
+        rotated_bullet = transform.rotate(self.image, self.angle)
+        window.blit(rotated_bullet, (self.rect.x, self.rect.y))
+
---
+++ classes/map_renderer.py
@@ -0,0 +1,38 @@
+from classes.brick_wall import BrickWall
+from classes.iron_wall import IronWall
+from classes.bush import Bush
+
+
+class MapRenderer:
+    def __init__(self, map_data, block_size):
+        self.map_data = map_data
+        self.block_size = block_size
+        self.blocks = []
+        self.current_x = 0
+        self.current_y = 0
+
+    # Функція для відображення карти
+    def draw_map(self):
+        with open(self.map_data, "r") as file:
+            map = file.readlines()
+            for line in map:
+                for letter in line:
+                    if letter == "W":
+                        self.blocks.append(
+                            BrickWall(self.current_x, self.current_y, "images/brick_wall_test.jpg", self.block_size,
+                                      self.block_size))
+                    elif letter == "I":
+                        self.blocks.append(
+                            IronWall(self.current_x, self.current_y, "images/iron_wall_test.jpg", self.block_size,
+                                     self.block_size))
+                    elif letter == "B":
+                        self.blocks.append(Bush(self.current_x, self.current_y, "images/bush_test.jpg", self.block_size,
+                                                self.block_size))
+                    elif letter == "_":
+                        self.current_x += self.block_size
+                        continue  # Продовжуємо наступну ітерацію циклу без виконання подальшого коду
+                    self.current_x += self.block_size
+
+                self.current_y += self.block_size
+                self.current_x = 0
+
